329382428d5d3d9698fcf0d7d5d03e86f2b253f2
Avatar <Avatar@AlphaGo.local>
2017-01-15 22:41:53 +0800

    ini
    Signed-off-by: Avatar <Avatar@AlphaGo.local>

---
+++ podTestLibrary/Classes/Test1.h
@@ -0,0 +1,13 @@
+//
+//  Test1.h
+//  afwq
+//
+//  Created by Avatar on 17/1/14.
+//  Copyright © 2017年 Avatar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test1 : NSObject
++ (void)abc;
+@end
---
+++ podTestLibrary/Classes/Test1.m
@@ -0,0 +1,15 @@
+//
+//  Test1.m
+//  afwq
+//
+//  Created by Avatar on 17/1/14.
+//  Copyright © 2017年 Avatar. All rights reserved.
+//
+
+#import "Test1.h"
+
+@implementation Test1
++ (void)abc {
+    NSLog(@"%@",@"aaa...");
+}
+@end
